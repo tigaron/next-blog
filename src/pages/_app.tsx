2d99fa52a7b6e6c9c830279e5a338e9a94bc9104
tigaron <suputra@mail.com>
2022-09-30 01:45:12 +0800

    feat: add tailwindcss configuration and add graphql dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import 'tailwindcss/tailwind.css';
+import '@/styles/globals.scss';
+import React from 'react';
+import Layout from '@/components/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
